d9fc66c00e0e014f6763966b2c8af12972ef07cd
rad48 <alexandre.huet48@gmail.com>
2020-09-16 17:03:23 +0200

    procfile and requirements

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ company = st.text_input('Company name', 'Google')
 import requests
 
 # url = 'https://myapp.herokuapp.com'
-url = 'http://127.0.0.1:8080/predict'
+url = 'https://deeptechpredict.herokuapp.com/predict'
 
 params = {
     'name' : company
