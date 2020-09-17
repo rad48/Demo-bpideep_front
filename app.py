f05ba48997b9e467194973e1f307d5f39534b5e2
rad48 <alexandre.huet48@gmail.com>
2020-09-17 16:48:50 +0200

    add search function

--- app.py
+++ app.py
@@ -2,23 +2,56 @@
 import json
 import requests
 import streamlit as st
+import pandas as pd
 
 # to launch front server:
 # streamlit run app.py
 
-company = st.text_input('Company name')
+# if st.button('Search company'):
+add_selectbox = st.sidebar.selectbox(
+    'Choose',
+    ('Predict', 'Search')
+)
 
-if company:
-    url = 'https://deeptechpredict.herokuapp.com/predict'
+if add_selectbox == 'Predict':
+    company = st.text_input('Company name')
 
-    params = {
-        'name' : company
-    }
+    if company:
+        url = 'https://deeptechpredict.herokuapp.com/predict'
 
-    response = requests.get(url, params)
+        params = {
+            'name' : company
+        }
 
-    resp = response.content
+        response = requests.get(url, params)
 
-    a = json.loads(resp)
-    st.write(a['predictions'])
+        resp = response.content
+
+        a = json.loads(resp)
+        st.write(a['predictions'])
+
+elif add_selectbox == 'Search':
+    year = st.text_input('Year')
+    month = st.text_input('Month')
+
+    if year and month:
+        # url = 'https://deeptechpredict.herokuapp.com/search'
+        url = 'http://127.0.0.1:8080/search'
+        params = {
+            'year' : year,
+            'month': month
+        }
+
+        response = requests.get(url, params)
+
+        resp = response.content
+
+        a = json.loads(resp)
+
+        df = pd.DataFrame.from_dict(a)
+        st.dataframe(df)
+
+
+
+
 
